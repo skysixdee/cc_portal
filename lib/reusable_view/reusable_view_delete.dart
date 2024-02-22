3e791542df0d8a8745472927bbed5ba0975ec2f1
gokul <gokulhariram2@gmail.com>
2024-02-22 16:48:27 +0530

    resuable

--- lib/reusable_view/reusable_view_delete.dart
+++ lib/reusable_view/reusable_view_delete.dart
@@ -1,4 +1,121 @@
+
 import 'package:flutter/material.dart';
+
+
+class TextFieldView extends StatelessWidget {
+  final String title;
+  final String hintText;
+  final bool addDropDown;
+  final bool isTextView;
+  final bool crossButton;
+  TextFieldView({
+    super.key, 
+    required this.hintText,
+    required this.title, 
+    this.addDropDown = false,
+    this.isTextView = false,
+    this.crossButton=false,
+  });
+
+  
+  final TextEditingController textFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left:10),
+      child: Container(
+          height: 150,
+          width: 300,
+          //color: Colors.blue,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color:Colors.grey,
+                      width:1
+                    ),
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                              height: isTextView ? double.minPositive : 55,
+                              color: Colors.red,
+                              width: 3
+                      ),        
+                      Expanded(
+                        child: TextField(
+                            maxLines: isTextView?null:1,
+                            controller: textFieldController,
+                            decoration: InputDecoration(
+                                hintText: hintText,
+                                hintStyle:TextStyle(
+                                  color:Color.fromARGB(255, 196, 193, 193),
+                                ),
+                                border: InputBorder.none,
+                                suffixIcon: Builder(builder: (context) {
+                                  if (crossButton==true) {
+                                    return IconButton(
+                                           onPressed: (){
+                                          textFieldController.clear();
+                                        } , 
+                                        icon:Icon(Icons.clear));
+                                  }
+                                  else{
+                                    return SizedBox();
+                                  }
+                                })
+                                )
+                              ),         
+                      ),
+                      addDropDown ?  DropdownWidget() : SizedBox(),
+                    ],
+                  ),
+                )
+              ])),
+    );
+  }
+}
+
+class DropdownWidget extends StatelessWidget {
+  const DropdownWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+             onPressed: (){
+               print('Dropdown button pressed');                
+              }, 
+              icon:Icon(Icons.arrow_drop_down)
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:sm_admin_portal/utilily/colors.dart';
 
@@ -37,4 +154,4 @@ class delete_view extends StatelessWidget {
       ),
     );
   }
-}
+} */
--- lib/screens/home_sceen.dart
+++ lib/screens/home_sceen.dart
@@ -1,33 +1,37 @@
 import 'package:flutter/material.dart';
+
 import 'package:sm_admin_portal/reusable_view/reusable_view_delete.dart';
-import 'package:sm_admin_portal/utilily/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: black12,
-      child: Row(
-        children: [
-          Column(
-            children: [
-              delete_view(title: "Pack name"),
-              delete_view(
-                title: "CRBT Type",
-                addDropDown: true,
+    return Scaffold(
+      body:Container(
+        child:Column(
+          children: [
+            TextFieldView( 
+              title: 'Pack Name',
+              hintText:'Enter Pack Name',
+              crossButton: true,
               ),
-              delete_view(title: "PACK Description"),
-              // Container(
-              //   color: redColor,
-              //   height: 200,
-              //   width: 200,
-              // ),
-            ],
-          ),
-        ],
-      ),
+            TextFieldView(
+              title: 'RBT Type',
+              hintText:'Enter RBT Type',
+              addDropDown: true,  
+              crossButton: true,
+            ),
+            TextFieldView(
+              title: 'PACK Description',
+              hintText:'Enter Pack Description',
+              isTextView: true,
+              crossButton: true,
+            )
+
+
+
+          ],),)
     );
   }
 }
