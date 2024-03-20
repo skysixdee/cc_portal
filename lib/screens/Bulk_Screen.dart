3e4a11b22b36a38822073434e4461938f7a483d0
bhavya <bhavya.revansiddapa@6dtech.co.in>
2024-03-20 11:43:29 +0530

    bulk

--- lib/screens/Bulk_Screen.dart
+++ lib/screens/Bulk_Screen.dart
@@ -32,7 +32,7 @@ class BulkScreen extends StatelessWidget {
                       children: [
                         ReusbaleDropDownButton(
                             items: ["ACTIVATION", "DEACTIVATION"],
-                            title: "Operation Name",
+                            title: "Operationn",
                             onChanged: (value) {
                               // if (value == "ACTIVATION") {
                               // Add your action here when "ACTIVATION" is selected
